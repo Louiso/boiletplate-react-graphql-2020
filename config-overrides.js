4ba302d748043cf83dbfdb15c118d40befc44ea6
Louiso <Gransullca.25@gmail.com>
2020-03-20 23:06:26 -0500

    [update] se agrego eslint jest

---
+++ config-overrides.js
@@ -0,0 +1,20 @@
+const { override, fixBabelImports, addWebpackAlias } = require('customize-cra')
+const { _moduleAliases } = require('./package.json')
+
+const aliases = {}
+
+Object.keys(_moduleAliases).forEach(key => {
+  aliases[key] = _moduleAliases[key].replace('.', __dirname)
+})
+
+module.exports = override(
+  addWebpackAlias({
+    ...aliases,
+    'react-dom': '@hot-loader/react-dom'
+  }),
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+    style: 'css',
+  })
+)
