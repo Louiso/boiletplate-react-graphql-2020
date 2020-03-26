fc182d85237cc8817dc15a9b8e9442c6962b6d60
luissullca <luis.sullca.h@uni.pe>
2020-03-26 16:58:47 -0500

    [update]

--- src/containers/home/child/Calendario.js
+++ src/containers/home/child/Calendario.js
@@ -1,42 +1,10 @@
 import React from 'react'
-import { Box, IconButton } from '@material-ui/core'
-import { Add as AddIcon } from '@material-ui/icons'
-// import { useQuery } from '@apollo/react-hooks'
-// import { GET_SCHEDULES } from 'apollo/home/types'
-// import { ListItem, List, ListItemText, Box } from '@material-ui/core'
-// <List>
-//         {cursos.map((curso, index) => {
-//           return (
-//             // eslint-disable-next-line react/no-array-index-key
-//             <ListItem key={index}>
-//               <ListItemText>{curso.code}</ListItemText>
-//             </ListItem>
-//           )
-//         })}
-//       </List>
-// const { data, loading } = useQuery(GET_SCHEDULES)
-// if(loading) return (<div>Loading ...</div>)
+import { Box } from '@material-ui/core'
 
-// const { getSchedules: cursos } = data
-// const Calendario = () => {
-
-//   return (
-//     <Box height="100%" overflow="auto" />
-//   )
-// }
-
-class Calendario extends React.Component {
-  render() {
-    const count = 0
-    return (
-      <div>
-        <IconButton>
-          <AddIcon />
-        </IconButton>
-        {count}
-      </div>
-    )
-  }
+const Calendario = () => {
+  return (
+    <Box height="100%" overflow="auto" />
+  )
 }
 
 export default Calendario
